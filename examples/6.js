d972dc2d6ed380167896cc0cee9c1a2129093e02
tristan <atdyer@ncsu.edu>
2016-12-21 22:21:41 -0500

    Add example 6, working on resizing bug

---
+++ examples/6.js
@@ -0,0 +1,34 @@
+var parseTime = d3.timeParse( '%d-%b-%y' );
+
+d3.tsv( 'data/apple_stock.tsv', function ( d ) {
+
+    d.date = parseTime( d.date );
+    d.close = +d.close;
+    return d;
+
+}, function ( error, data ) {
+
+    if ( error ) throw error;
+
+    var c = chart()
+        .x( function ( d ) { return d.date; } )
+        .y( function ( d ) { return d.close; } )
+        .x_scale( d3.scaleTime() );
+
+    c.line()
+        .data( data )
+        .thickness( 1.5 );
+
+    resize();
+
+    function resize () {
+
+        var width = d3.select( 'body' ).node().getBoundingClientRect().width;
+        c.width( width );
+        d3.select( '#chart' ).call( c );
+
+    }
+
+    d3.select( window ).on( 'resize', resize );
+
+});
