156c7e4d63a9fb948c66f035daa59bf4206270d7
Tristan <atdyer@ncsu.edu>
2016-12-29 16:28:17 -0500

    Add example 11

---
+++ examples/11.js
@@ -0,0 +1,36 @@
+var parseTime = d3.timeParse('%d-%b-%y');
+
+d3.tsv('data/apple_stock.tsv', function ( d ) {
+
+    d.date = parseTime(d.date);
+    d.close = +d.close;
+    return d;
+
+}, function ( error, data ) {
+
+    if ( error ) throw error;
+
+    var width = d3.select('body')
+                  .node()
+                  .getBoundingClientRect().width;
+
+    var c = chart()
+        .width(width)
+        .margin({right: 30})
+        .x(function ( d ) { return d.date; })
+        .y(function ( d ) { return d.close; })
+        .x_axis(d3.axisBottom())
+        .x_location('80%')
+        .x_grid(true)
+        .y_axis(d3.axisRight())
+        .y_location(new Date('2010', '06'))
+        .y_grid(true)
+        .x_scale(d3.scaleTime());
+
+    c.scatter()
+     .data(data)
+     .radius(1.5);
+
+    d3.select('#chart')
+      .call( c );
+});
